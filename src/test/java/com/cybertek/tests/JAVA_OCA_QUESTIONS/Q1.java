0df812837095fe4d277490595e3910b0421c5c64
faceflame <ozcansahin0654@gmail.com>
2019-11-27 19:16:19 -0500

    First IntelliJ Commit

---
+++ src/test/java/com/cybertek/tests/JAVA_OCA_QUESTIONS/Q1.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.JAVA_OCA_QUESTIONS;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+
+
+        int [] [] arr = new int [][] {{1,2,}, {3,4}};
+
+        arr [1]=arr[0];
+
+        System.out.println(arr[1]);
+
+    }
+
+
+}
---
+++ src/test/java/com/cybertek/tests/JAVA_OCA_QUESTIONS/Q6.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.JAVA_OCA_QUESTIONS;
+
+public class Q6 {
+    public static void main(String[] args) {
+
+        System.out.println("Resut A" + 0 + 1);
+        System.out.println("resultB" +(0) + (1));
+
+        System.out.println( 0 + 1 +"Result" + (0) +(1));
+
+    }
+}
