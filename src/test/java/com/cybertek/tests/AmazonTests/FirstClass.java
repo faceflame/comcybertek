d9726a7c4eda5cfd19cb3ee00ad3bf7cee6c0288
faceflame <55003341+faceflame@users.noreply.github.com>
2019-12-05 15:28:46 -0500

    Update FirstClass.java

--- src/test/java/com/cybertek/tests/AmazonTests/FirstClass.java
+++ src/test/java/com/cybertek/tests/AmazonTests/FirstClass.java
@@ -2,6 +2,8 @@ package com.cybertek.tests.AmazonTests;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("First Class");
+        System.out.println("Hello now First Class");
+        
+        System.out.println("Change in remote repository");
     }
 }
