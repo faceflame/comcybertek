ccf25453656e79c0060f6f0f164fa6ddf2fab249
faceflame <ozcansahin0654@gmail.com>
2019-12-05 15:48:35 -0500

    'change'

--- src/test/java/com/cybertek/tests/AmazonTests/FirstClass.java
+++ src/test/java/com/cybertek/tests/AmazonTests/FirstClass.java
@@ -3,7 +3,9 @@ package com.cybertek.tests.AmazonTests;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello now First Class");
-        
         System.out.println("Change in remote repository");
+        System.out.println("Another Line");
+        System.out.println("Another Line");
+        System.out.println("Another Line");
     }
 }
